401ff1387b45e211d4e164f5b137cce6df13ed33
Stan Marian <117512836+MarianCristianStan@users.noreply.github.com>
2026-05-06 23:33:20 +0300

    fix port cloud issue

--- RobotShop/Program.cs
+++ RobotShop/Program.cs
@@ -6,12 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddProjectServices(builder.Configuration);
 builder.Services.AddControllersWithViews();
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
-
-
 var app = builder.Build();
-
 
 if (!app.Environment.IsDevelopment())
 {
